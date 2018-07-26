084dda2350d4638c0dc4b58ae123269b7af3a4f6
srinivasbestcodes <srini.villuri@gmail.com>
2018-07-26 17:14:46 +0530

    routescreate and values get by express and bodyparser moduel

--- server.js
+++ server.js
@@ -1,5 +1,44 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var app = express();
+app.set('view engine', 'ejs');
+//app.use(express.bodyParser());
+var url = "mongodb://localhost:27017/test";
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/add', function(req , res) {
+    res.render('add');
+});
+
+app.post('/insert', function(req , res) {
+    console.log("say something");
+    console.log(req.body.fname);
+    console.log(req.body.lname);
+    res.render('add');
+});
+
+
+
+
+//mongoose.connect(url, { useNewUrlParser: true } ,function(err, db) {
+  //  if (err){ console.log("error:"+err);return}
+    //console.log("Database created!");
+   //const user={name:'swarup',dept:'it',age:23}
+    //db.collection("people").insertOne(user, function(err, res) {
+      //  if (err) throw err;
+        //console.log("1 document inserted");
+        //db.close();
+    //});
+
+//});
+
 app.listen(8000, function() {
-    console.log('listening on 3000')
+    console.log('listening on 8000')
 })
